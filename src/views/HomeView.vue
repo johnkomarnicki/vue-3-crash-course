64ae56edfa5e7b7f556d6d344a08e6be6dc35545
John <john@johnkomarnicki.com>
2023-01-09 19:58:18 -0500

    section-1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
