b8398ff5dc908b31a31f1dc0850447fc394c76fd
John <john@johnkomarnicki.com>
2023-01-09 20:23:39 -0500

    section-4

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
+import TodoHeader from "./components/TodoHeader.vue";
 </script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
 
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="@/assets/Vue_Logo_Black.png" alt="" />
+        <h1>Vue Todos</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink :to="{ name: 'todos' }">Todos</RouterLink>
+        <RouterLink :to="{ name: 'about' }">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
