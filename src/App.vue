59c76fa4b5a4bba44ecc1b3d301cd38392d88eb0
John <john@johnkomarnicki.com>
2023-01-09 20:07:16 -0500

    section-3

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,18 @@ import { RouterView } from "vue-router";
   <RouterView />
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap");
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
