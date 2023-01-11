e972287b98783c2cd1dfa12ee1b3993ca9aa1465
John <john@johnkomarnicki.com>
2023-01-10 23:07:18 -0500

    section-6

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,13 +1,20 @@
 <script setup>
 import { ref } from "vue";
 
+const emit = defineEmits(["create-todo"]);
+
 const todo = ref("");
+
+const createTodo = () => {
+  emit("create-todo", todo.value);
+  todo.value = "";
+};
 </script>
 
 <template>
   <div class="input-wrap">
     <input type="text" v-model="todo" />
-    <button>Create</button>
+    <button @click="createTodo()">Create</button>
   </div>
 </template>
 
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { uid } from "uid";
+import { ref } from "vue";
 import TodoCreator from "../components/TodoCreator.vue";
+
+const todoList = ref([]);
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: false,
+    isEditing: null,
+  });
+};
 </script>
 
 <template>
   <main>
     <h1>Create Todo</h1>
-    <TodoCreator />
+    <TodoCreator @create-todo="createTodo" />
   </main>
 </template>
 
