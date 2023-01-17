a87b0d5ceecfd46d523521d97a11d2ce740351fe
John <john@johnkomarnicki.com>
2023-01-17 13:10:17 -0500

    section-9

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,7 +2,7 @@
 import { uid } from "uid";
 import { ref } from "vue";
 import TodoCreator from "../components/TodoCreator.vue";
-import TodoList from "../components/TodoList.vue";
+import TodoItem from "../components/TodoItem.vue";
 
 const todoList = ref([]);
 const createTodo = (todo) => {
@@ -21,7 +21,13 @@ const createTodo = (todo) => {
     <TodoCreator @create-todo="createTodo">
       <template #button-content>Create</template>
     </TodoCreator>
-    <TodoList :todoList="todoList" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -37,6 +43,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
